82c8bc7d56c5e117780a706faece6e7d002ac014
Eman <eman.edrees.kanaan@gmail.com>
2022-03-18 16:16:07 +0300

    maintenances

--- lib/computer.dart
+++ lib/computer.dart
@@ -63,16 +63,52 @@ class _ComputerState extends State<Computer> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  margin: EdgeInsets.only(right: 20,left: 20,top:50),
-                  height: 700,
-                  width: double.infinity,
-                  child: Text(''),
-                  decoration: BoxDecoration(
-                      color:Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 20,left: 20,top:50),
+                      height: 700,
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          Container(
+                            child:Image.asset("assets/image/computer.png",width: 85, height: 85),
+                          ),
+                          Container(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                labelText: "اسم الموظف",
+                                labelStyle: TextStyle(
+                                  color:Color(0xff022C43),
+                                  fontFamily: 'Cairo',
+                                  fontSize: 20,
+
+                                ),
+
+                              ),
+
+                            ),
+                          ),
+                          Container(
+                            child:  ElevatedButton(onPressed: (){
+                              Navigator.pushNamed(context,'/');
+                            }, child: Text('ارسال الطلب',style: TextStyle(fontFamily: 'Cairo',fontSize: 17,color: Colors.white ),),
+                              style: ButtonStyle(backgroundColor:
+                              MaterialStateProperty.all( Color(0xff26ADD9)), padding:MaterialStateProperty.all(EdgeInsets.only(left: 30,right: 30,top:5,bottom: 5)) ,shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(borderRadius: BorderRadius.circular(14.0),),),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color:Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
 
     ),
+                    ),
+
+                  ],
                 ),
               ],
             ),
